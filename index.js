28e9f1e983a9483762912c18ddc3c29d1f891686
Weilbyte <git@weilbyte.dev>
2020-08-17 11:21:52 +0200

    Run instead of returning endpoint functions

--- index.js
+++ index.js
@@ -3,20 +3,13 @@ const sites = require('./sites');
 const list = require('./api/list');
 const upload = require('./api/upload');
 
-
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
 exports.requestEntry = (req, res) => {
     switch (req.path) {
         case '/upload':
-            return upload(req, res);
+            upload(req, res);
             break;
         case '/list':
-            return list(req, res);
+            list(req, res);
             break;
         default:
             res.status(404).send(JSON.stringify({
