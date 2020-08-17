b8b798d781b69c3dd33072a9af47b00023394ea0
Weilbyte <git@weilbyte.dev>
2020-08-17 17:33:46 +0200

    Debug form data

--- api/upload.js
+++ api/upload.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const IncomingForm = require('formidable').IncomingForm;
 
-async function handleUnwanted(req, res) {
+async function handleUnwanted(req, res, data) {
     if (req.method !== "POST") {
       res.statusCode = 405
       await res.send(
@@ -24,48 +24,51 @@ async function handleUnwanted(req, res) {
         return true
     }
 
+    if (data.files === undefined) {
+        await res.status(400).send(JSON.stringify({
+            success: false, 
+            msg: "Unexpected error at form-data parsing."
+        }));
+    }
+
+    if (!data.files["image"]) {
+        res.statusCode = 400
+        await res.send(
+          JSON.stringify({
+            success: false,
+            msg: "Missing 'image' form file.",
+          })
+        )
+        return true
+      }
+    
+      if (!data.files["image"]["type"].startsWith("image/")) {
+        res.statusCode = 400
+        await res.send(
+          JSON.stringify({
+            success: false,
+            msg: "Uploaded file must be image/* type.",
+          })
+        )
+        return true
+      }
+
     return false
 };
 
-async function handleUnwantedData(res, data) {
-    if (!data.files["image"]) {
-      res.statusCode = 400
-      await res.send(
-        JSON.stringify({
-          success: false,
-          msg: "Missing 'image' form file.",
-        })
-      )
-      return true
-    }
-  
-    if (!data.files["image"]["type"].startsWith("image/")) {
-      res.statusCode = 400
-      await res.send(
-        JSON.stringify({
-          success: false,
-          msg: "Uploaded file must be image/* type.",
-        })
-      )
-      return true
-    }
-
-    return false;
-};
-
 exports.default = async function (req, res) {
-    const unwanted = await handleUnwanted(req, res);
-    if (unwanted) return;
-
     const data = await new Promise((resolve, reject) => {
         const form = new IncomingForm();
         form.parse(req, (err, fields, files) => {
-          if (err) return reject(err);
+          if (err) {
+              console.log(err);
+              return resolve(undefined);
+          };
           resolve({ fields, files });
         })
     });
 
-    unwanted = await handleUnwantedData(res, data);
+    const unwanted = await handleUnwanted(req, res, data);
     if (unwanted) return;
 
     console.log('prep fs read')
